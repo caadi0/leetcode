22c4026862a0479a593ce7d2908416c159c978bc
Odin\Odin1 <chauhanaaditya0@gmail.com>
2017-01-11 20:32:50 -0500

    Reverse Integer with Overflow check

---
+++ 7. Reverse Integer.java
@@ -0,0 +1,17 @@
+public class Solution {
+    public int reverse(int x) {
+        
+        int sign = (x > 0 ? 1 : -1);
+        x *= sign;
+        int ans = 0;
+        
+        do {
+            if (ans > (Integer.MAX_VALUE - x % 10) / 10)
+                return 0;
+            ans = ans * 10 + x % 10;
+            x /= 10;
+        } while (x > 0);
+        
+        return ans * sign;
+    }
+}
