58772560549e94df4560edf188c7c82f3ab39110
Odin\Odin1 <chauhanaaditya0@gmail.com>
2017-01-25 09:55:26 -0500

    MSB edge case

---
+++ 190. Reverse Bits.java
@@ -0,0 +1,13 @@
+public class Solution {
+    // you need treat n as an unsigned value
+    public int reverseBits(int n) {
+        int num = 0;
+        for(int i = 0; i < 32; i++) {
+            num += n & 1;
+            if(i < 31)
+                num = num << 1;
+            n = n >>> 1;
+        }
+        return num;
+    }
+}
